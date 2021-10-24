cf1b11a00c3eeed4279598a980c70cd769f3d116
waqassaid <waqassaid1234@gmail.com>
2021-10-24 08:03:05 +0500

    chapter4completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: "Awesome App",
-    home: HomePage(),
-  ));
+  runApp(
+    const HomePage(),
+  );
 }
 
 class HomePage extends StatelessWidget {
@@ -13,43 +12,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.yellow, title: const Text("AWesome App")),
-        body: Container(
-          color: Colors.black,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                child: Center(child: Text("Waqas")),
-                width: 100,
-                height: 100,
-                color: Colors.green,
-              ),
-              Container(
-                child: Center(
-                  child: Text(
-                    "Imran",
-                  ),
-                ),
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ),
-              Container(
-                child: Center(
-                    child: Text(
-                  "Ali",
-                )),
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              )
-            ],
-          ),
-        ));
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+    );
   }
 }
